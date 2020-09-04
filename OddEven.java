c0f6024d0cf60ef47181b577f021cd7235184342
pari_pakhi <adwikaparimal@gmail.com>
2020-09-04 13:30:45 +0530

    OddEven.java added

---
+++ OddEven.java
@@ -0,0 +1,35 @@
+<<<<<<< HEAD
+import.java.util*;
+
+class OddEven{
+  public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    int no;
+    System.out.println("Enter no");
+    no=sc.nextInt;
+    if(no%2==1){
+      System.out.println("Odd");
+    }
+    else{
+      System.out.println("Even");
+    }
+  }
+}
+=======
+import.java.util*;
+
+class OddEven{
+  public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    int no;
+    System.out.println("Enter no");
+    no=sc.nextInt;
+    if(no%2==1){
+      System.out.println("Odd");
+    }
+    else{
+      System.out.println("Even");
+    }
+  }
+}
+>>>>>>> c27cec859c2de3f4941fa6f5e3a9225adebe1dab
