7728e72b1f79be6e4deeeee76565801367e3d1b3
pari_pakhi <adwikaparimal@gmail.com>
2020-09-04 13:38:05 +0530

    basic java program

---
+++ SmallerBigger.java
@@ -0,0 +1,10 @@
+class SmallerBigger{
+  Public static void main(int a, int b){
+    If(a>b){
+      System.out.println("a is greater than b. Thus, b is the smaller number.")
+    }
+    ElseIf(b>a){
+      System.out.println("b is greater than b. Thus, a is the smaller number.")
+    }
+  }
+}
